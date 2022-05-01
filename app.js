255633d608357e07edfa96d9143d0114d6dc6cce
TRV <lstxvrt@gmail.com>
2022-05-01 20:06:58 +0530

    Add div with app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const parentContainer = document.getElementById("container");
+
+for (let i = 0; i < 256; i++) {
+
+  const pixel = document.createElement("div")
+  parentContainer.append(pixel);
+
+}
